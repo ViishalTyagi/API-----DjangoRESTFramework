23190320aa0ea00cdf3b754774b89a2fc9ea39e5
Vishal Tyagi <viishaltyagi@gmail.com>
2019-01-21 03:25:33 -0500

    Data Representations via Serializers

--- drfapi/urls.py
+++ drfapi/urls.py
@@ -14,8 +14,15 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from rest_framework import routers
+from apidrf.views import UserViewSet, GroupViewSet
+
+router = routers.DefaultRouter()
+router.register('users', UserViewSet)
+router.register('groups', GroupViewSet)
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('', include(router.urls)),
+    path('api', include('rest_framework.urls', namespace='rest_framework'))
 ]
